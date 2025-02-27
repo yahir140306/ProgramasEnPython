13ab5d84f137760ecb7c18b85ca26ef7b7eac33c
yahir140306 <joseyahirhernandez11@gmail.com>
2025-02-27 09:09:35 -0600

    Correciones

--- condicionalesIfElse/Dobles/elMayor.py
+++ condicionalesIfElse/Dobles/elMayor.py
@@ -8,9 +8,12 @@ numero3 = int(input("Ingrese el tercero número: "))
 
 if numero1 > numero2 and numero1 > numero3:
     print(f"El número {numero1} es mayor que el número {numero2} y {numero3}")
+
 elif numero2 > numero1 and numero2 > numero3:
     print(f"El número {numero2} es mayor que el número {numero1} y {numero3}")
+
 elif numero3 > numero1 and numero3 > numero2:
     print(f"El número {numero3} es mayor que el número {numero1} y {numero2}")
+
 else:
     print(f"Los números {numero1}, {numero2} y {numero3} son iguales")
