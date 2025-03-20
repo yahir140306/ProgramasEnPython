5988fb11cbd5da600cf178ff646cf7d98a35a4a4
yahir140306 <joseyahirhernandez11@gmail.com>
2025-03-19 20:19:50 -0600

    Acomodar estructura

--- ciclo/7_2_tabla_multiplicar_while.py
+++ ciclo/7_2_tabla_multiplicar_while.py
@@ -1,12 +1,14 @@
-print('Crear un programa el cual imprima las tablas de multiplicar del 1 al 10.')
+print("Crear un programa el cual imprima las tablas de multiplicar del 1 al 10.")
 
 tabla = 1
+
 while tabla < 11:
-    print(f'\nTabla del {tabla}')
+    
+    print(f"\nTabla del {tabla}")
     multiplicador = 1
+
     while multiplicador < 11:
-        print(f'{tabla} x {multiplicador} = {tabla * multiplicador}')
+        print(f"{tabla} x {multiplicador} = {tabla * multiplicador}")
         multiplicador += 1
+
     tabla += 1
-
-
