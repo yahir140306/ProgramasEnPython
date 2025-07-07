ce5323449ae1f2dbb2f74fcbd3ba9b15aeb39004
yahir140306 <joseyahirhernandez11@gmail.com>
2025-07-07 10:32:23 -0600

    Add data insertion and deletion functions to database connection application

--- 3_cuatrimestre/tkinter_BD/20_conexion.py
+++ 3_cuatrimestre/tkinter_BD/20_conexion.py
@@ -41,9 +41,46 @@ def mostrar_datos():
         print(fila)
 
 
+def insertar_datos():
+    cn = mysql.connector.connect(
+        host="localhost",
+        port=3306,
+        user="root",
+        password="",
+        database="farmacia_juan_yahir",
+    )
+    cursor1 = cn.cursor()
+    sql = "insert into productos_farmacia (id_producto, nombre, precio) values(%s, %s, %s)"
+    datos = (12343, "Ibuprofeno", 50.00)
+    cursor1.execute(sql, datos)
+    cursor1.close()
+    cn.commit()
+    print("Datos registrados")
+
+
+def eliminar_registro():
+    cn = mysql.connector.connect(
+        host="localhost",
+        port=3306,
+        user="root",
+        password="",
+        database="farmacia_juan_yahir",
+    )
+    cursor1 = cn.cursor()
+    sql = "delete from productos_farmacia where id_producto = %s"
+    datos = (12343,)
+    cursor1.execute(sql, datos)
+    cursor1.close()
+    cn.commit()
+    print("Registro eliminado")
+
+
 btnConection = tk.Button(frameConection, text="Conectar BD", command=conecctionBD)
 btnConection.pack(pady=15)
 btnConection = tk.Button(frameConection, text="Seleccionar", command=mostrar_datos)
+btnConection.pack(pady=15)
+
+btnConection = tk.Button(frameConection, text="Registrar", command=insertar_datos)
 btnConection.pack(pady=15)
 
 # conecctionBD()
